da07a92609c99bf6fed5439860595915e93579f7
Patrick Smith <patrick@burntcaramel.com>
2021-04-25 12:59:22 +1000

    Make focus test stop complaining

---
+++ src/focus.ts
@@ -0,0 +1,12 @@
+// describe("focus", () => {
+	// function* Modal(content) {
+	// 	const closeButton = yield canTab(button("Close"));
+	// 	yield focus(closeButton);
+		
+	// 	yield content;
+		
+	// 	yield onFocus(el => {
+	// 		el.
+	// 	});
+	// }
+// });
